0a8f38008548d787357c0eb5b76675ee26b58c5d
sarki012 <60797267+sarki012@users.noreply.github.com>
2024-05-20 14:07:28 -0600

    Run node index.js and Shows App is Running

--- js_temp/index.js
+++ js_temp/index.js
@@ -1,3 +1,14 @@
-function changeBackgroundColor() {
+/*function changeBackgroundColor() {
     document.getElementById("divsection").style.backgroundColor = "green";
- }
+ }*/
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the homepage.");
+});
+
+app.listen(3000, () => {
+  console.log("App is running!");
+});
